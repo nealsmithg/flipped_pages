d046df67509f7ff9fa059b95d0e10840098f5ac1
Cornelius Smith <nealsmithg@gmail.com>
2021-11-06 12:18:07 -0400

    added post to userbooks

--- controllers/api/userBooksRoutes.js
+++ controllers/api/userBooksRoutes.js
@@ -23,6 +23,27 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {});
+router.post("/", (req, res) => {
+  // req.body should look like
+  // {
+  //     books_id: 4,
+  //     read: false,
+  //     own: true,
+  //     ---------
+  //     can have ..
+  //     want: true,
+  //     heard_about: "text text text"
+  // }
+  try {
+    const newUserBooks = req.body;
+    newUserBooks.user_id = req.session.user_id;
+    //TODO: delete on finish.
+    if (!req.session.user_id) newUserBooks.user_id = 3;
+    UserBooks.create(newUserBooks);
+    res.status(200).json(newUserBooks);
+  } catch (err) {
+    res.json(400).json(err);
+  }
+});
 
 module.exports = router;
