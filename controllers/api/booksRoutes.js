3ac0db3d1187e8b7f209df5a96cfdaade081b46a
Cornelius Smith <nealsmithg@gmail.com>
2021-11-06 10:50:00 -0400

    added get for books data

---
+++ controllers/api/booksRoutes.js
@@ -0,0 +1,45 @@
+const router = require("express").Router();
+const { Books } = require("../../models");
+
+router.get("/", async (req, res) => {
+  try {
+    const booksData = await Books.findAll();
+    res.status(200).json(booksData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const newBook = await Books.create({
+      ...req.body,
+    });
+
+    res.status(200).json(newBooks);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const projectData = await Books.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!projectData) {
+      res.status(404).json({ message: "No project found with this id!" });
+      return;
+    }
+
+    res.status(200).json(projectData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
