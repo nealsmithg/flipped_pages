176e36041788332248c43991d012b31e46016815
Cornelius Smith <nealsmithg@gmail.com>
2021-11-06 11:09:03 -0400

    added post and delete to books

--- controllers/api/booksRoutes.js
+++ controllers/api/booksRoutes.js
@@ -10,13 +10,23 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+  try {
+    const booksData = await Books.findByPk(req.params.id);
+    if (!booksData) {
+      res.status(404).json({ massage: "No book by that ID." });
+      return;
+    }
+    res.status(200).json(booksData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.post("/", async (req, res) => {
   try {
-    const newBook = await Books.create({
-      ...req.body,
-    });
-
-    res.status(200).json(newBooks);
+    const newBook = await Books.create(req.body);
+    res.status(200).json(newBook);
   } catch (err) {
     res.status(400).json(err);
   }
