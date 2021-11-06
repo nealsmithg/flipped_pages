43866beefcfcf01c0660770bd89a8820b3667950
Cornelius Smith <nealsmithg@gmail.com>
2021-11-06 11:13:49 -0400

    fixed delete in books router

--- controllers/api/booksRoutes.js
+++ controllers/api/booksRoutes.js
@@ -34,19 +34,18 @@ router.post("/", async (req, res) => {
 
 router.delete("/:id", async (req, res) => {
   try {
-    const projectData = await Books.destroy({
+    const booksData = await Books.destroy({
       where: {
         id: req.params.id,
-        user_id: req.session.user_id,
       },
     });
 
-    if (!projectData) {
-      res.status(404).json({ message: "No project found with this id!" });
+    if (!booksData) {
+      res.status(404).json({ message: "No book found with this id!" });
       return;
     }
 
-    res.status(200).json(projectData);
+    res.status(200).json(booksData);
   } catch (err) {
     res.status(500).json(err);
   }
